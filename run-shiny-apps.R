e83f95734ea4fd1ea73d716d5197552c2ba8d5dd
Tobin Magle <tobin.magle@colostate.edu>
2018-08-06 15:04:37 -0600

    added run apps script and getting started with shinyio

---
+++ run-shiny-apps.R
@@ -0,0 +1,8 @@
+#Run app from a local directory
+runApp(appDir = "rmacc-shiny")
+
+#Run app from a zip file on the web
+runUrl( "https://github.com/maglet/rmacc-shiny/blob/master/rmacc-shiny.zip")
+
+#Run app from a git repository
+runGitHub( "rmacc-shiny", "maglet")
